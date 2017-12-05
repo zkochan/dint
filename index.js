b29eba5504cafa2a652223ee77617a6c8f58ca47
Zoltan Kochan <z@kochan.io>
2017-12-05 22:40:41 +0200

    refactor: use mz/p-limit instead of BlueBird

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict'
 
-const BB = require('bluebird')
-const fs = BB.promisifyAll(require('graceful-fs'))
+const fs = require('mz/fs')
 const ssri = require('ssri')
 const path = require('path')
 const pEvery = require('p-every')
+const pLimit = require('p-limit')
+
+const limit = pLimit(20)
 
 const MAX_BULK_SIZE = 1 * 1024 * 1024 // 1MB
 
@@ -13,22 +15,24 @@ function generateFrom (dirname) {
 }
 
 function _generateFrom (file, fname) {
-  return fs.statAsync(file).then(stat => {
+  return fs.stat(file).then(stat => {
     if (stat.isDirectory()) {
-      return BB.map(fs.readdirAsync(file), f => {
-        return _generateFrom(path.join(file, f), path.join(fname, f))
-      }, {concurrency: 20}).then(files => {
-        return files.reduce((acc, info) => {
-          if (info) {
-            Object.assign(acc, info)
-          }
-          return acc
-        }, {})
-      })
+      return fs.readdir(file)
+        .then(files => Promise.all(files.map(f => limit(() => {
+          return _generateFrom(path.join(file, f), path.join(fname, f))
+        }))))
+        .then(files => {
+          return files.reduce((acc, info) => {
+            if (info) {
+              Object.assign(acc, info)
+            }
+            return acc
+          }, {})
+        })
     } else if (!stat.isFile()) {
       // ignored. We don't do things like symlinks rn
     } else if (stat.size < MAX_BULK_SIZE) {
-      return fs.readFileAsync(file)
+      return fs.readFile(file)
         .then(data => ssri.fromData(data))
         .then(integrity => ({[fname]: {integrity: integrity.toString(), size: stat.size}}))
     } else {
@@ -56,7 +60,7 @@ function check (dirname, dirIntegrity) {
         })
     }
 
-    return fs.readFileAsync(filename)
+    return fs.readFile(filename)
       .then(data => ssri.checkData(data, fstat.integrity))
       .catch(err => {
         if (err.code === 'EINTEGRITY' || err.code === 'ENOENT') return false
