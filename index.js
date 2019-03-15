bac740b1e921631c95321b60b1c6c64d09b57ecb
Zoltan Kochan <zoltan.kochan@gmail.com>
2019-03-15 23:22:29 +0200

    feat: use es2017 syntax

--- index.js
+++ index.js
@@ -14,42 +14,39 @@ function generateFrom (dirname) {
   return _retrieveFileIntegrities(dirname, dirname, {})
 }
 
-function _retrieveFileIntegrities (rootDir, currDir, index) {
-  return fs.readdir(currDir)
-  .then((files) => {
-    return Promise.all(files.map((file) => {
+async function _retrieveFileIntegrities (rootDir, currDir, index) {
+  try {
+    const files = await fs.readdir(currDir)
+    await Promise.all(files.map(async (file) => {
       const fullPath = path.join(currDir, file)
-      return fs.stat(fullPath)
-      .then((stat) => {
-        if (stat.isDirectory()) {
-          return _retrieveFileIntegrities(rootDir, fullPath, index)
+      const stat = await fs.stat(fullPath)
+      if (stat.isDirectory()) {
+        return _retrieveFileIntegrities(rootDir, fullPath, index)
+      }
+      if (stat.isFile()) {
+        const relativePath = path.relative(rootDir, fullPath)
+        index[relativePath] = {
+          size: stat.size,
+          generatingIntegrity: limit(() => {
+            return stat.size < MAX_BULK_SIZE
+              ? fs.readFile(fullPath).then(ssri.fromData)
+              : ssri.fromStream(fs.createReadStream(fullPath))
+          })
         }
-        if (stat.isFile()) {
-          const relativePath = path.relative(rootDir, fullPath)
-          index[relativePath] = {
-            size: stat.size,
-            generatingIntegrity: limit(() => {
-              return stat.size < MAX_BULK_SIZE
-                     ? fs.readFile(fullPath).then(ssri.fromData)
-                     : ssri.fromStream(fs.createReadStream(fullPath))
-            })
-          }
-        }
-      })
+      }
     }))
-  })
-  .then(() => index)
-  .catch(err => {
+    return index
+  } catch (err) {
     if (err.code !== 'ENOENT') {
       throw err
     }
     return index
-  })
+  }
 }
 
 function check (dirname, dirIntegrity) {
   dirname = path.resolve(dirname)
-  return pEvery(Object.keys(dirIntegrity), f => {
+  return pEvery(Object.keys(dirIntegrity), async (f) => {
     const fstat = dirIntegrity[f]
 
     // TODO: return something else to distinguish an integrity mismatch from
@@ -58,19 +55,21 @@ function check (dirname, dirIntegrity) {
 
     const filename = path.join(dirname, f)
     if (fstat.size > MAX_BULK_SIZE) {
-      return ssri.checkStream(fs.createReadStream(filename), fstat.integrity)
-        .catch(err => {
-          if (err.code === 'EINTEGRITY' || err.code === 'ENOENT') return false
-          throw err
-        })
-    }
-
-    return fs.readFile(filename)
-      .then(data => ssri.checkData(data, fstat.integrity))
-      .catch(err => {
+      try {
+        return await ssri.checkStream(fs.createReadStream(filename), fstat.integrity)
+      } catch (err) {
         if (err.code === 'EINTEGRITY' || err.code === 'ENOENT') return false
         throw err
-      })
+      }
+    }
+
+    try {
+      const data = await fs.readFile(filename)
+      return ssri.checkData(data, fstat.integrity)
+    } catch (err) {
+      if (err.code === 'EINTEGRITY' || err.code === 'ENOENT') return false
+      throw err
+    }
   }, {concurrency: 100})
 }
 
--- test/index.js
+++ test/index.js
@@ -3,47 +3,23 @@ const test = require('tape')
 const dint = require('dint')
 const path = require('path')
 
-test('generate and verify', t => {
+test('generate and verify', async (t) => {
   const dirname = path.join(__dirname, 'fixtures', '1')
 
-  let dirIntegrity
-  dint.from(dirname)
-    .then(_dirIntegrity => Promise.all(
-        Object.keys(_dirIntegrity).map(filename => _dirIntegrity[filename].generatingIntegrity.then(integrity => ({filename, integrity})))
-      )
-    )
-    .then(_dirIntegrity => {
-      dirIntegrity = _dirIntegrity.reduce((acc, file) => Object.assign(acc, {[file.filename]: {integrity: file.integrity}}), {})
+  const dirIntegrityPending = await dint.from(dirname)
+  const dirIntegrityArr = await Promise.all(
+    Object.keys(dirIntegrityPending).map(filename => dirIntegrityPending[filename].generatingIntegrity.then(integrity => ({filename, integrity})))
+  )
+  const dirIntegrity = dirIntegrityArr.reduce((acc, file) => Object.assign(acc, {[file.filename]: {integrity: file.integrity}}), {})
 
-      t.ok(dirIntegrity['foo.js'])
-      t.ok(dirIntegrity['lib/bar.js'])
+  t.ok(dirIntegrity['foo.js'])
+  t.ok(dirIntegrity['lib/bar.js'])
 
-      return dint.check(dirname, dirIntegrity)
-    })
-    .then(ok => {
-      t.ok(ok, 'no changes')
+  t.ok(await dint.check(dirname, dirIntegrity), 'no changes')
+  t.notOk(await dint.check(path.join(__dirname, 'fixtures', '2'), dirIntegrity), 'small file differs')
+  t.notOk(await dint.check(path.join(__dirname, 'fixtures', '3'), dirIntegrity), 'big file differs')
+  t.notOk(await dint.check(path.join(__dirname, 'fixtures', '4'), dirIntegrity), 'file is missing')
+  t.notOk(await dint.check(path.join(__dirname, 'fixtures', '3'), {'foo.js': {}}), 'integrity is missing')
 
-      return dint.check(path.join(__dirname, 'fixtures', '2'), dirIntegrity)
-    })
-    .then(ok => {
-      t.notOk(ok, 'small file differs')
-
-      return dint.check(path.join(__dirname, 'fixtures', '3'), dirIntegrity)
-    })
-    .then(ok => {
-      t.notOk(ok, 'big file differs')
-
-      return dint.check(path.join(__dirname, 'fixtures', '4'), dirIntegrity)
-    })
-    .then(ok => {
-      t.notOk(ok, 'file is missing')
-
-      return dint.check(path.join(__dirname, 'fixtures', '3'), {'foo.js': {}})
-    })
-    .then(ok => {
-      t.notOk(ok, 'integrity is missing')
-
-      t.end()
-    })
-    .catch(t.end)
+  t.end()
 })
